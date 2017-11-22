06b38a7dd94f48b9b2b7429b44b447a517e0dad7
Belén González Baradit <belen.gonzalez.12@sansano.usm.cl>
2017-11-22 17:29:15 -0300

    Testing complete for GET/folders: id_parent_folder, limit, offset

--- spec/api/folders_spec.rb
+++ spec/api/folders_spec.rb
@@ -58,7 +58,95 @@ RSpec.describe 'API Folder Resourse', type: :request do
         end
       end
     end
+
+    context 'return folders that are children of the ID' do
+      before(:each) do
+        @father = FactoryBot.create(:folder, :has_children, name: 'padre', family_tree: [{type:1},{type:1},{type:1},{type:1}])
+        get "/api/v1/folders?id_parent_folder =#{@father.id}"
+      end
+
+      it 'should returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+
+      it 'should be a Json Array' do
+        expect{JSON.parse(response.body)}.not_to raise_error
+        expect(response.content_type).to eq("application/json")
+      end
+
+      it 'should contain 4' do
+        #expect(response.count).to contain_exacly(3)
+        expect(JSON.parse(response.body).count).to eq(4)
+      end
+      it 'should contain all accesable children folders of the ID father ' do
+        accesable = Folder.child_of([@father.id])
+        response_ids = JSON.parse(response.body).map{ |a| a['id']}
+        accesable.each do |folder|
+          expect(response_ids).to include(folder.id)
+        end
+      end
+    end
+
+    context 'return a limit number of folders' do
+      before(:each) do
+        @father = FactoryBot.create(:folder, :has_children, family_tree: [{type:1},{type:1},{type:1},{type:1}])
+        get "/api/v1/folders?limit =2"
+      end
+
+      it 'should returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+
+      it 'should be a Json Array' do
+        expect{JSON.parse(response.body)}.not_to raise_error
+        expect(response.content_type).to eq("application/json")
+      end
+
+      it 'should contain 2' do
+        #expect(response.count).to contain_exacly(2)
+        expect(JSON.parse(response.body).count).to eq(2)
+      end
+      it 'should show a limit of folders' do
+        accesable = Folder.limit(2)
+        response_ids = JSON.parse(response.body).map{ |a| a['id']}
+        accesable.each do |folder|
+          expect(response_ids).to include(folder.id)
+        end
+      end
+    end
+
+    context 'return the remaining folder from the offset' do
+      before(:each) do
+        @father = FactoryBot.create(:folder, :has_children, family_tree: [{type:1},{type:1},{type:1},{type:1}])
+        get "/api/v1/folders?offset =3"
+      end
+
+      it 'should returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+
+      it 'should be a Json Array' do
+        expect{JSON.parse(response.body)}.not_to raise_error
+        expect(response.content_type).to eq("application/json")
+      end
+
+      it 'should contain 2' do
+        #expect(response.count).to contain_exacly(2)
+        expect(JSON.parse(response.body).count).to eq(2)
+      end
+      it 'should show a limit of folders' do
+        accesable = Folder.offset(3)
+        response_ids = JSON.parse(response.body).map{ |a| a['id']}
+        accesable.each do |folder|
+          expect(response_ids).to include(folder.id)
+        end
+      end
+    end
   end
+
+  
+
+
 end
 
 
