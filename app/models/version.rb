4234d4894a6bdd428c0c026264a938b779b8976b
Teodoro Hochfärber <Teohoch2@gmail.com>
2017-11-23 04:05:05 -0300

    Removed Keys

--- app/models/version.rb
+++ app/models/version.rb
@@ -3,12 +3,4 @@ class Version < ApplicationRecord
   belongs_to :user
   mount_uploader :upfile, UpfileUploader
 
-  before_destroy :destroy_cloud
-
-  private
-
-  def destroy_cloud
-    # TODO Implement Function that deletes a certain version of a file from the cloud
-    sleep(0.1)
-  end
 end
